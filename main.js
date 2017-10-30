b1a3c442ad795e6411baba7633a3de9946e96838
CurtLiao <32745146+CurtLiao@users.noreply.github.com>
2017-10-30 20:40:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a').click(function(){return false;});
+
+	$('.box').click(function(){
+
+		var value = $(this).attr('rel');
+		$(this).effect(value,'slow').effect('slide');
+
+	});
+
+
+});
